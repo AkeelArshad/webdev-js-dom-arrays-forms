ae2c9cc9779cea305c9920b5d40c535338a11b3f
Arshad <c2007010@hallam.shu.ac.uk>
2023-11-10 12:34:57 +0000

    Created Javascript MouseEvent

--- js/main.js
+++ js/main.js
@@ -6,8 +6,11 @@
     document.querySelector('nav ul li').setAttribute('class', 'currentPage');
     
     //adding events
-    document.getElementById("myTestEvent").addEventListener("click", function() {
+    //document.getElementById("myTestEvent").addEventListener("click", function() {
         console.info("hi I was clicked");
+    })
+    document.getElementById('myTestEvent').addEventListener('click', function(ev){
+        console.dir(ev);
     })
     
     // End coding here
