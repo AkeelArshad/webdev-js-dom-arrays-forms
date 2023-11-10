105f8719a70966ae14b1384d516c2addf3ff0e63
Arshad <c2007010@hallam.shu.ac.uk>
2023-11-10 12:28:03 +0000

    fixed spelling of current and created an event.

--- js/main.js
+++ js/main.js
@@ -3,6 +3,12 @@
     //Start coding here
     document.getElementById("myHeading").innerHTML = "Akeel";
     
-    document.querySelector('nav ul li').setAttribute('class', 'currnetPage')
+    document.querySelector('nav ul li').setAttribute('class', 'currentPage');
+    
+    //adding events
+    document.getElementById("myTestEvent").addEventListener("click", function() {
+        console.info("hi I was clicked");
+    })
+    
     // End coding here
 })();
