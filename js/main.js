19f6587e880676492b4dea1e7d8c16db1f452395
Arshad <c2007010@hallam.shu.ac.uk>
2023-11-17 11:15:42 +0000

    Changed the background colours when clicked

--- js/main.js
+++ js/main.js
@@ -9,20 +9,6 @@
     document.getElementById("myTestEvent").addEventListener("click", function() {
         console.info("hi I was clicked");
     })
-    document.getElementById('myTestEvent').addEventListener('click', function(ev){
-        console.dir(ev);
-    })
-    document.querySelector(".red").addEventListener('click', function(ev){
-        document.querySelector('body').setAttribute('class', "redBack");
-    })
-    document.querySelector(".blue").addEventListener('click', function(ev){
-        document.querySelector('body').setAttribute('class', "blueBack");
-    })
-    document.querySelector(".green").addEventListener('click', function(ev){
-        document.querySelector('body').setAttribute('class', "greenBack");
-    })
-    document.querySelector(".reset").addEventListener('click', function(ev){
-        document.querySelector('body').removeAttribute('class');
-    })
+    
     // End coding here
 })();
